29d78b9d16bf4a2534e6b4a2717d67acbcc4e456
keval <167281609+Krkotadiya123@users.noreply.github.com>
2024-12-12 11:34:16 +0530

    Update settings.py

--- mybatchproject/settings.py
+++ mybatchproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@ux5+mo3t_%l!cfuv3at=w)!zm%0lv0#pb*+uwc-3gs1apume8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = flase
 
 ALLOWED_HOSTS = ['*']
 
